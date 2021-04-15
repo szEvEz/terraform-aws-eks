b3a39294f2ea323e2c2a12e8145250057bad0ac0
szEvEz <szivos.john@gmail.com>
2021-04-15 12:13:36 +0200

    Remove terraform lock file, make sure the same vpc module version is used

--- examples/secrets_encryption/main.tf
+++ examples/secrets_encryption/main.tf
@@ -57,7 +57,7 @@ resource "aws_kms_key" "eks" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.47.0"
+  version = "2.78.0"
 
   name                 = "test-vpc"
   cidr                 = "10.0.0.0/16"
